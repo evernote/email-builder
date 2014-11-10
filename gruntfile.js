732fc32a733626b2677185c59e8e89b648cddaaa
Ryan <rburgess@evernote.com>
2014-11-10 11:25:36 -0800

    add gruntfile and package file

---
+++ gruntfile.js
@@ -0,0 +1,136 @@
+module.exports = function(grunt) {
+
+  var globalConfig = {
+    language: 'en',
+    data: 'psurvey',
+    template: 'psurvey',
+    directory: 'survey'
+  };
+
+  if(grunt.option('lang') != undefined){
+    globalConfig.language = grunt.option('lang');
+  }
+
+  if(grunt.option('data') != undefined){
+    globalConfig.data = grunt.option('data');
+  }
+
+  if(grunt.option('template') != undefined){
+    globalConfig.template = grunt.option('template');
+  }
+
+  if(grunt.option('directory') != undefined){
+    globalConfig.directory = grunt.option('directory');
+  }
+
+  grunt.initConfig({
+    globalConfig: globalConfig,
+    sass: {
+        dist: {
+            options: {
+              //outputStyle: 'compressed'
+            },
+            files: [
+              {
+                expand: true,
+                cwd: 'sass/',
+                src: ['**/*.scss'],
+                dest: 'build/css/',
+                ext: '.css',
+              },
+            ],
+        }
+    },
+    juice: {
+      options: {
+        removeStyleTags: false
+      },
+      dynamic_mappings: {
+         files: [
+           {
+             expand: true,
+             cwd: 'build/**/*',
+             src: ['**/*.html'],
+             dest: 'build/**/*',
+             ext: '.html'
+           }
+         ]
+      }
+     },
+     'compile-handlebars': {
+         allStatic: {
+          templateData: 'emails/content/<%= globalConfig.directory %>/<%= globalConfig.data %>-<%= globalConfig.language %>.json',
+          template: 'emails/templates/<%= globalConfig.directory %>/<%= globalConfig.template %>.handlebars',
+          output: 'build/<%= globalConfig.directory %>/<%= globalConfig.data %>-<%= globalConfig.language %>.html',
+          globals: [
+             'emails/content/footers/legal-footer-<%= globalConfig.language %>.json'
+          ],
+        },
+      },
+    jsonlint: {
+      sample: {
+        src: [ 'emails/content/**/*.json' ]
+      }
+    },
+     prettify: {
+      options: {
+        indent: 3,
+        indent_char: ' ',
+        wrap_line_length: 500,
+        brace_style: 'expand',
+        unformatted: ['a', 'sub', 'sup', 'b', 'i', 'u']
+      },
+      all: {
+        expand: true,
+        cwd: 'build/',
+        ext: '.html',
+        src: ['**/*.html'],
+        dest: 'build/'
+      },
+    },
+     watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass:dist'],
+        options: { 
+            spawn: false
+        },
+      },
+      handlebars:{
+        files: ['emails/templates/**/*.handlebars'],
+        tasks: ['newer:compile-handlebars'],
+        options: { 
+            spawn: false
+        },
+      },
+      html:{
+        files: ['build/**/*.html'],
+        tasks: ['newer:juice', 'newer:prettify'],
+        options: { 
+            spawn: false
+        },
+      },
+      json: {
+        files: ['emails/content/**/*'],
+        tasks: ['newer:jsonlint'],
+        options: {
+          spawn: false
+        }
+      },
+      livereload: {
+        files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+        options: {
+           livereload: true
+        }
+      }
+    }
+   });
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-prettify');
+    grunt.loadNpmTasks('grunt-compile-handlebars');
+    grunt.loadNpmTasks('grunt-juice-email');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-jsonlint');
+    grunt.registerTask('default',['watch']);
+};
